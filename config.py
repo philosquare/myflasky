afc4e6d6388e87a48753e5402fe75fbc0758dbfe
lfz <a188616786a@163.com>
2017-03-20 17:04:09 +0800

    to yun

--- config.py
+++ config.py
@@ -7,12 +7,12 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     FLASKY_ADMIN = 'a188616786a@163.com'
-    MAIL_SERVER = 'smtp.csu.edu.cn'
+    MAIL_SERVER = 'smtp.163.com'
     MAIL_PORT = 25
     MAIL_USE_TLS = False
-    MAIL_USERNAME = 'a188616786a@csu.edu.cn'
+    MAIL_USERNAME = 'a188616786a@163.com'
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SENDER = 'me <a188616786a@csu.edu.cn>'
+    FLASKY_MAIL_SENDER = 'me <a188616786a@163.com>'
     FLASKY_POSTS_PER_PAGE = 10
     FLASKY_COMMENTS_PER_PAGE = 10
 
